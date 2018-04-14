9019fcaf9ac45b0a4479024d687644ce96c8aa37
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-04-15 00:54:16 +0900

    UIImagePickerViewを使用して、ライブラリから選択した画像をUIImageViewに表示　追加

--- UseImagePickerView/Classes/Controllers/ViewController.swift
+++ UseImagePickerView/Classes/Controllers/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlet
+    
+    @IBOutlet private var imageView: UIImageView!
+    
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Actions
+    
+    @IBAction private func cameraButtonTapped(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
-
-
+    
 }
+
+// MARK: - UIImagePickerControllerDelegate
+/*******************************************************************************************/
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        let editImage = info[UIImagePickerControllerEditedImage] as? UIImage
+        let originImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView.image = editImage != nil ? editImage : originImage
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
